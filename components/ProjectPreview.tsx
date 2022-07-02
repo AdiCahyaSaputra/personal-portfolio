3136e7256b69f97f1ae7fa04adf5fc69a9d3adad
AdiCahyaSaputra <adi.JSONparse@gmail.com>
2022-07-02 22:27:05 +0700

    fix height yang berlebihan

--- components/ProjectPreview.tsx
+++ components/ProjectPreview.tsx
@@ -29,15 +29,15 @@ const ProjectPreview: React.FC = () => {
 		<>
 			<div className={`fixed transition-all ${closeOverlay} inset-0 bg-black/75 z-40`}>
 
-				<div className={`fixed overflow-y-auto z-50 p-4 delay-200 transition-all duration-300 overflow-hidden ${closeProjectPreview ? '-left-20 w-0' : 'w-10/12 md:w-5/12'} inset-y-0 bg-white/30 backdrop-blur-md`}>
+				<div className={`fixed overflow-y-auto z-50 p-4 delay-200 transition-all duration-300 overflow-hidden ${closeProjectPreview ? '-left-20 w-0' : 'w-10/12 md:w-4/12'} inset-y-0 bg-white/30 backdrop-blur-md`}>
 					<header>
 						<div className={`w-full flex justify-center items-center transition-all duration-100 ${closeProjectPreview && '-translate-x-full'} aspect-video bg-white ${loading && 'animate-pulse'} mx-auto`}>
 							{ !loading && ( <Image src={context?.data.projectImage!} width={1280} height={720}/> ) }
 						</div>
 					</header>
 					<article>
-						<h1 className={`mt-4 ${loading && 'p-4 bg-white/60 animate-pulse'} transition-all ${closeProjectPreview && '-translate-x-full'} text-white font-mono font-bold text-xl md:text-2xl`}>{ !loading && context?.data.projectName }</h1>
-						<p className={`mt-2 ${loading && 'p-2 bg-gray-500 animate-pulse'} transition-all ${closeProjectPreview && '-translate-x-full'} text-white md:text-lg font-light`}>{ !loading && context?.data.projectDesc }</p>
+						<h1 className={`mt-4 ${loading && 'p-4 bg-white/60 animate-pulse'} transition-all ${closeProjectPreview && '-translate-x-full'} text-white font-mono font-bold text-xl md:text-lg`}>{ !loading && context?.data.projectName }</h1>
+						<p className={`mt-2 ${loading && 'p-2 bg-gray-500 animate-pulse'} transition-all ${closeProjectPreview && '-translate-x-full'} text-white md:text-sm font-light`}>{ !loading && context?.data.projectDesc }</p>
 						<div onClick={() => router.push(context?.data.projectURL!)} className={`group flex ${!loading && 'w-max'} transition-all duration-200 ${closeProjectPreview && '-translate-x-full' } items-center mt-6 bg-black`}>
 							<FrameworkIcons priority={false} srcIcon='/icons/github.svg' bg="bg-black" size={18} />
 							<p className={`group-hover:text-white/60 ${loading && 'p-2 bg-black animate-pulse'} pr-3 font-mono text-white text-sm line-clamp-1`}>{ !loading && 'Check On Github' }</p>
