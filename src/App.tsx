3be8d61a61565bcfa1ca551afdc588327b5e84a1
AdiCahyaSaputra <adi.JSONparse@gmail.com>
2022-12-11 13:35:14 +0700

    feat: typing animation

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
 
         <main className="text-white min-h-screen flex flex-col justify-center bg-black">
 
-          <div className='bg-black/30 select-none backdrop-blur-md sticky top-0 p-4'>
+          <div className='bg-black/30 z-50 select-none backdrop-blur-md sticky top-0 p-4'>
             <Container>
               <Logo toRight='left-0' textSize='text-3xl' barHeight='h-1 md:h-1.5' />
             </Container>
@@ -16,14 +16,20 @@ const App = () => {
 
           <div className='p-4'>
             <Container>
-              <h1 className='selection:text-black selection:bg-white text-lg font-mono text-white font-extrabold md:text-lg'>
+
+              <h1 className='
+                selection:text-black selection:bg-white text-lg font-mono text-white font-extrabold md:text-lg
+                overflow-hidden whitespace-nowrap border-r-8 pr-2 border-white w-max animate-typing
+              '>
                 {"Just A Fullstuck Developer From Indonesia"}
               </h1>
+
               <div className='select-none mt-4 space-y-2 text-white/60 font-medium'>
                 <p>{"I'm Vocational High School Student Of Software Engineer,"}</p>
                 <p>{"Interest In Frontend Development, UI & UX Design, And Problem Solving."}</p>
                 <p>{"Open To Collaborate With Your Team :)"}</p>
               </div>
+
             </Container>
           </div>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./src/**/*.{html,js, ts, jsx, tsx}",
-		"./src/**/*"
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: [
+    "./src/**/*.{html,js, ts, jsx, tsx}",
+    "./src/**/*"
+  ],
+  theme: {
+    extend: {
+      // animation: {
+      //   typing: 'typing 1.8s ease-out .8s 1 normal both'
+      // },
+      // keyframes: {
+      //   typing: {
+      //     '0%': { width: '0ch' },
+      //     '5%, 10%': { width: '1ch' },
+      //     '15%, 20%': { width: '2ch' },
+      //     '25%, 30%': { width: '3ch' },
+      //     '35%, 40%': { width: '4ch' },
+      //     '45%, 50%': { width: '5ch' },
+      //     '55%, 60%': { width: '6ch' },
+      //     '65%, 70%': { width: '7ch' },
+      //     '75%, 80%': { width: '8ch' },
+      //     '85%, 90%': { width: '9ch' },
+      //     '95%': { width: '10ch' },
+      //   }
+      // }
+    },
+  },
+  plugins: [],
 }
