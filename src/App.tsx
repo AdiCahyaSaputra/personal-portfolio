5dafe3c9345f8d24243d53642f1edb39585f1ef5
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2022-12-16 10:16:06 +0700

    style: hero section style for typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,50 @@
-import Logo from './components/reusable/Logo'
-import Container from './components/reusable/Container'
+import Logo from "./components/reusable/Logo"
+import Container from "./components/reusable/Container"
+import { useEffect, useState } from "react"
 
 const App = () => {
+  const [toRight, setToRight] = useState("-left-full")
+
+  useEffect(() => {
+    setTimeout(() => {
+      setToRight("left-0")
+    }, 500)
+  }, [toRight])
+
   return (
     <>
-      <div className='bg-black'>
+      <div className="bg-black">
 
         <main className="text-white min-h-screen flex flex-col justify-center bg-black">
 
-          <div className='bg-black/30 z-50 select-none backdrop-blur-md sticky top-0 p-4'>
+          <div className="bg-black/30 z-50 select-none backdrop-blur-md sticky top-0 p-4">
             <Container>
-              <Logo toRight='left-0' textSize='text-3xl' barHeight='h-1 md:h-1.5' />
+              <Logo toRight={toRight} textSize="text-3xl" barHeight='h-1 md:h-1.5' />
             </Container>
           </div>
 
-          <div className='p-4'>
+          <div className="p-4">
             <Container>
 
-              <h1 className='
+              <h1 className="
                 selection:text-black selection:bg-white text-lg font-mono text-white font-extrabold md:text-lg
-                overflow-hidden whitespace-nowrap border-r-8 pr-2 border-white w-max animate-typing
-              '>
+              ">
                 {"Just A Fullstuck Developer From Indonesia"}
               </h1>
 
-              <div className='select-none mt-4 space-y-2 text-white/60 font-medium'>
+              <div className="select-none my-4 space-y-2 text-white/60 font-medium">
                 <p>{"I'm Vocational High School Student Of Software Engineer,"}</p>
                 <p>{"Interest In Frontend Development, UI & UX Design, And Problem Solving."}</p>
-                <p>{"Open To Collaborate With Your Team :)"}</p>
               </div>
+
+              <button className="transition-all duration-500 hover:shadow-md hover:shadow-white/40 py-2 px-4 bg-white text-black font-bold">{"Open To Collaborate With Your Team :)"}</button>
 
             </Container>
           </div>
 
         </main>
 
-        <div className='h-screen bg-green-600'>
+        <div className="h-screen bg-green-600">
 
         </div>
 
